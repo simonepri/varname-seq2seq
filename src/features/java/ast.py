10190103889e0895aeb64c28119af9326e0e3cbe
simonepri <simonepri@outlook.com>
2020-02-08 14:01:55 +0000

    Fix compilation cmd

--- src/features/java/ast.py
+++ src/features/java/ast.py
@@ -220,7 +220,7 @@ class JavaAst:
             "-Xmaxerrs",
             "10000000",
             "-Xplugin:FeaturePlugin",
-            *file_paths,
+            *map(lambda f: os.path.relpath(f), file_paths)
         ]
         try:
             check_call(cmd, stdout=DEVNULL, stderr=STDOUT)

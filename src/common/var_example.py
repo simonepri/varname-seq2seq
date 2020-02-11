26bf13ec51be803b34249732b7cb081118b65974
simonepri <simonepri@outlook.com>
2020-02-11 17:29:21 +0000

    Add script to tokenize examples

--- src/common/var_example.py
+++ src/common/var_example.py
@@ -1,4 +1,7 @@
+import ast
 from typing import *
+
+from transformers import PreTrainedTokenizer
 
 from utils.strings import multiple_replace
 
@@ -61,3 +64,88 @@ class VarExample:
     def __decode_token(cls, token: str) -> str:
         map = {r"\n": "\n", r"\r": "\r", r"\t": "\t"}
         return multiple_replace(map, token)
+
+
+class TokenizedVarExample:
+    def __init__(self, multi_tokens: List[List[str]], masks: List[int]) -> None:
+        assert len(multi_tokens) == len(masks)
+        self.multi_tokens = multi_tokens
+        self.masks = masks
+
+    def __iter__(self) -> Iterable[Tuple[List[str], int]]:
+        return zip(self.multi_tokens, self.masks)
+
+    def __len__(self) -> int:
+        return len(self.multi_tokens)
+
+    def tlen(self) -> int:
+        if self.tlen is not None:
+            return self.tlen
+        self.tlen = 0
+        for multi_token in self.multi_tokens:
+            self.tlen += len(multi_token)
+        return self.tlen
+
+    @classmethod
+    def from_var_example(
+        cls, example: VarExample, tokenizer: Callable[[str], List[str]]
+    ) -> "TokenizedVarExample":
+        tlen = 0
+        multi_tokens = []
+        for token, varid in example:
+            multi_token = tokenizer(token)
+            multi_tokens.append(multi_token)
+            tlen += len(multi_token)
+        tokenized_example = cls(multi_tokens, example.masks)
+        tokenized_example.tlen = tlen
+        return tokenized_example
+
+    @classmethod
+    def from_var_examples(
+        cls, examples: List[VarExample], tokenizer: Callable[[str], List[str]]
+    ) -> List["TokenizedVarExample"]:
+        tokenized_examples = []
+        for example in examples:
+            tokenized_example = cls.from_var_example(example, tokenizer)
+            tokenized_examples.append(tokenized_example)
+        return tokenized_examples
+
+    @classmethod
+    def serialize(cls, tokenized_example: "TokenizedVarExample") -> str:
+        tokens = []
+        for multi_token, _ in tokenized_example:
+            token = "\t".join(multi_token)
+            tokens.append(token)
+        example = VarExample(tokens, tokenized_example.masks)
+        return VarExample.serialize(example)
+
+    @classmethod
+    def deserialize(cls, text: str) -> "TokenizedVarExample":
+        example = VarExample.deserialize(text)
+        tlen = 0
+        multi_tokens = []
+        for token, _ in example:
+            multi_token = token.split("\t")
+            multi_tokens.append(multi_token)
+            tlen += len(multi_token)
+        tokenized_example = cls(multi_tokens, example.masks)
+        tokenized_example.tlen = tlen
+        return tokenized_example
+
+    @classmethod
+    def serialize_to_file(
+        cls, file_path: str, examples: List["TokenizedVarExample"]
+    ) -> None:
+        with open(file_path, "w+") as f:
+            for example in examples:
+                print(cls.serialize(example), file=f)
+
+    @classmethod
+    def deserialize_from_file(
+        cls, file_path: str
+    ) -> List["TokenizedVarExample"]:
+        examples = []
+        with open(file_path, "r") as f:
+            for line in f:
+                examples.append(cls.deserialize(line))
+        return examples
